23a45ff4c6ffb3cad0b9126d0bc847e0bbc3733d
Richard Hadzhiev <ri4koto100@gmail.com>
2024-04-19 15:07:24 +0200

    fix:display player and obstacles in the middle

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 class Player{
     constructor(){
-        this.positionX = 50;
-        this.positionY = 0;
         this.width = 10;
         this.height = 10;
+        this.positionX = 50 - this.width / 2;
+        this.positionY = 0;
 
         this.playerElm = document.getElementById("player")
-        this.playerElm.style.left = this.positionX + "vw"
-        this.playerElm.style.bottom = this.positionY + "vh"
         this.playerElm.style.width = this.width + "vw";
         this.playerElm.style.height = this.height + "vh";
+        this.playerElm.style.left = this.positionX + "vw"
+        this.playerElm.style.bottom = this.positionY + "vh"
 
 
     }
@@ -34,10 +34,10 @@ class Player{
 
 class Obstacle{
     constructor(){
-        this.positionX = 50;
-        this.positionY = 85;
         this.width = 10;
         this.height = 10;
+        this.positionX = 50 - this.width/2;
+        this.positionY = 85;
         this.obstacleElm = null;
 
         this.createDomElement();
@@ -47,10 +47,10 @@ class Obstacle{
         //creating a property
         this.obstacleElm = document.createElement("div");
         this.obstacleElm.className = "obstacle";
-        this.obstacleElm.style.left = this.positionX + "vw";
-        this.obstacleElm.style.bottom = this.positionY + "vh";
         this.obstacleElm.style.width = this.width + "vw"
         this.obstacleElm.style.height = this.height + "vh";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
 
         
         const parentElm = document.getElementById("board");
