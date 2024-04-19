caaa56bcc5f5e9f00fa756a2f1282ae0c5a0dbc3
Richard Hadzhiev <ri4koto100@gmail.com>
2024-04-19 13:00:16 +0200

    implement Obstacles

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ class Player{
     constructor(){
         this.positionX = 50;
         this.positionY = 0;
-        this.width = 20;
-        this.heigth = 20;
+        this.width = 10;
+        this.heigth = 10;
 
         this.playerElm = document.getElementById("player")
         this.playerElm.style.left = this.positionX + "vw"
@@ -27,7 +27,51 @@ class Player{
 }
 
 
+class Obstacle{
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 85;
+        this.width = 10;
+        this.heigth = 10;
+        this.obstacleElm = null;
+
+        this.createDomElement();
+
+    }
+    createDomElement(){
+        //creating a property
+        this.obstacleElm = document.createElement("div");
+        this.obstacleElm.className = "obstacle";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+        this.obstacleElm.style.width = this.width + "vw"
+        this.obstacleElm.style.height = this.heigth + "vh";
+
+        
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.obstacleElm);
+    }
+    moveDown(){
+        this.positionY--;
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+    }
+}
+
 const player = new Player()
+const obstacleArr = []; //instanced of class Obstacle
+//create new obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstacleArr.push(newObstacle)
+}, 2000);
+
+
+// move all obstacle
+setInterval(() => {
+    obstacleArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown();
+    })
+}, 30);
 
 document.addEventListener("keydown",(e) => {
     if(e.code === "ArrowLeft"){
